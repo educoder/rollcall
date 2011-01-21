53e32d2b1a6c997033bc22f18c9878ec5497b413
Matt Zukowski <matt@zukowski.ca>
2011-01-21 11:45:14 -0500

    moved custom responder setting out to application controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery
   layout 'application'
+  
+  # use the custom responder from RestfulJSONP to handle
+  # JSONP requests
+  self.responder = RestfulJSONP::JSONPResponder
 end
