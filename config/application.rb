a16df15ff7c45aa3e07cc71505cba0b89298c4c9
Matt Zukowski <matt@zukowski.ca>
2011-01-21 15:56:50 -0500

    filter out the 'token' parameter from logs

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module Rollcall
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :token]
   end
 end
