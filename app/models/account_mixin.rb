98ae67f883f3a57608acbfd26e3bb758f0c065fe
Matt Zukowski <matt@zukowski.ca>
2011-06-05 18:22:39 -0400

    accounts for groups are now optional

--- app/models/account_mixin.rb
+++ app/models/account_mixin.rb
@@ -7,12 +7,12 @@ module AccountMixin
       delegate :password, :to => :account
       delegate :encrypted_password, :to => :account
       
-      accepts_nested_attributes_for :account
+      accepts_nested_attributes_for :account, :allow_destroy => true
       
-      validates :account, :presence => true
+      #validates :account, :presence => true
       
       validate do
-        unless account.valid?
+        unless !account || account.valid?
           self.errors[:base] << "Account could not be #{account.new_record? ? "created" : "updated"}."
         end
       end
@@ -21,10 +21,12 @@ module AccountMixin
         # fix weird error messages caused by :autosave => true combined with errors.full_messages
         if self.errors[:"account.base"]
           base_errors = self.errors[:base]
-          account_base_errors = self.account.errors[:base]
-          self.errors[:base].clear
-          [base_errors + account_base_errors].each{|e| self.errors[:base] << e}
-          self.errors.delete(:"account.base")
+          if self.account
+            account_base_errors = self.account.errors[:base]
+            self.errors[:base].clear
+            [base_errors + account_base_errors].each{|e| self.errors[:base] << e}
+            self.errors.delete(:"account.base")
+          end
         end
       end
       
